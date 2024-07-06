77ee53a0dbccaa0679c3221eccc04d54895d4423
Chima Nwachukwu <tylerdgenius@Chimas-MBP.(none)>
2024-07-06 17:41:56 +0100

    feat: added code push init on App

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
 import CodePush from 'react-native-code-push';
 
 let codePushOptions = {checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME};
 
 const App = () => {
-  return <></>;
+  return <View />;
 };
 
 export default CodePush(codePushOptions)(App);
