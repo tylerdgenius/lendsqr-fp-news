1df8b3c953392111be943dfca5b6bc95427271fe
tylerdgenius <chima.nwachukwu@fairmoney.io>
2024-07-07 13:35:21 +0100

    feat: implemented screen tracking

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,16 +1,39 @@
-import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import analytics from '@react-native-firebase/analytics';
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
+import React, {useRef} from 'react';
 import {useAppSelector} from '../redux';
+import {RootStackParamsList} from '../types';
 import Base from './Base';
 import Unauthenticated from './Unauthenticated';
 
 const Entry = () => {
   const {user} = useAppSelector(state => state.userReducer);
 
-  console.log({user});
+  const routeNameRef = useRef<string | undefined>();
+  const navigationRef = useRef<NavigationContainerRef<RootStackParamsList>>();
 
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current?.getCurrentRoute()?.name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current?.getCurrentRoute()?.name;
+
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+
+        routeNameRef.current = currentRouteName;
+      }}>
       {user.email || user.id ? <Base /> : <Unauthenticated />}
     </NavigationContainer>
   );
